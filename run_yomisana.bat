5f48231f25319242aec9f926dd92c3df45e38436
yomisana <sammywhy3236@gmail.com>
2024-02-06 00:54:23 +0800

    init,nice beginning

---
+++ run_yomisana.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo.
+echo =========================================================================
+echo Yomisana GTX 1660 Ti Stable Diffusion For Windows protable python version
+echo 優米撒那 GTX 1660 Ti Stable Diffusion For Windows 免安裝 python 版本
+echo =========================================================================
+echo.
+
+@REM echo. 啟動中...
+@REM echo. 這可能需要一些時間，請稍等片刻...
+
+pause
