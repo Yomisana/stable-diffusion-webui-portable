e3045314b4a79ab9289ffc7aeba05d7725d85842
yomisana <sammywhy3236@gmail.com>
2024-02-06 02:17:22 +0800

    Update run_yomisana.bat script to set the correct paths and create venv if it doesn't exist

--- run_yomisana.bat
+++ run_yomisana.bat
@@ -8,21 +8,22 @@ echo =========================================================================
 echo.
 
 echo Current working directory: %cd%
+set ROOT=%cd%
+cd stable_diffusion_webui
+
+echo In Stable Diffusion WebUI working directory: %cd%
 
 @REM first set portable python path
-set PYTHON= %cd%\portable_python\python.exe
+set PYTHON=%ROOT%\portable_python\python.exe
 
 @REM check stable diffusion webui venv folder
-set venv_folder=%cd%\stable_diffusion_webui\venv
+set venv_folder=%ROOT%\stable_diffusion_webui\venv
 if exist %venv_folder% (
-    echo 已存在 venv
+    echo exist venv
 ) else (
-    echo 執行創建 venv
-    REM 在這裡加入創建 virtualenv  的命令，例如：python -m virtualenv  %venv_folder%
-    call %PYTHON% -m virtualenv %venv_folder%
-    copy /Y %targetBinPath%\python\python310.zip %venv_folder%\Scripts\
+    echo create venv
+    call %PYTHON% -m virtualenv venv
 )
-
 
 
 
@@ -39,7 +40,7 @@ set COMMANDLINE_ARGS= --xformers --disable-safe-unpickle --medvram
 @REM set COMMANDLINE_ARGS= --use-cpu all --no-half --skip-torch-cuda-test --enable-insecure-extension-access
 
 @REM Start Stable Diffusion WebUI
-call stable_diffusion_webui/webui.bat
+call webui.bat
 
 @REM if call webui.bat not work will pause
 pause
