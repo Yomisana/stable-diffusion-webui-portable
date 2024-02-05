a98250d67d74c56eb8f0e2eda1dafb3830d87e3d
yomisana <sammywhy3236@gmail.com>
2024-02-06 02:01:58 +0800

    update portable_python & bat

--- run_yomisana.bat
+++ run_yomisana.bat
@@ -7,7 +7,39 @@ echo 優米撒那 GTX 1660 Ti Stable Diffusion For Windows 免安裝 python 版本
 echo =========================================================================
 echo.
 
-@REM echo. 啟動中...
-@REM echo. 這可能需要一些時間，請稍等片刻...
+echo Current working directory: %cd%
 
+@REM first set portable python path
+set PYTHON= %cd%\portable_python\python.exe
+
+@REM check stable diffusion webui venv folder
+set venv_folder=%cd%\stable_diffusion_webui\venv
+if exist %venv_folder% (
+    echo 已存在 venv
+) else (
+    echo 執行創建 venv
+    REM 在這裡加入創建 virtualenv  的命令，例如：python -m virtualenv  %venv_folder%
+    call %PYTHON% -m virtualenv %venv_folder%
+    copy /Y %targetBinPath%\python\python310.zip %venv_folder%\Scripts\
+)
+
+
+
+
+
+@REM none install portable git
+@REM set GIT= 
+@REM default virtual environment directory
+@REM set VENV_DIR=
+@REM use GPU 6G Video RAM
+set COMMANDLINE_ARGS= --xformers --disable-safe-unpickle --medvram
+@REM @REM use less than 6G Video RAM
+@REM set COMMANDLINE_ARGS= --xformers --disable-safe-unpickle --lowvram
+@REM @REM use CPU only
+@REM set COMMANDLINE_ARGS= --use-cpu all --no-half --skip-torch-cuda-test --enable-insecure-extension-access
+
+@REM Start Stable Diffusion WebUI
+call stable_diffusion_webui/webui.bat
+
+@REM if call webui.bat not work will pause
 pause
